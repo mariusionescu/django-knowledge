33a06c0696bcb1f48397fb05e6e16b11ecade18c
root <root@dev.getcaster.com>
2018-08-23 13:19:41 +0000

    Fixed some urls, and templates.

--- knowledge/urls.py
+++ knowledge/urls.py
@@ -1,22 +1,25 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import  url
+
+from knowledge.views import (knowledge_index, knowledge_list, knowledge_thread,
+                    knowledge_moderate, knowledge_ask)
 
 urlpatterns = [
-    url(r'^$', 'knowledge_index', name='knowledge_index'),
+    url(r'^$', knowledge_index, name='knowledge_index'),
 
-    url(r'^questions/$', 'knowledge_list', name='knowledge_list'),
+    url(r'^questions/$', knowledge_list, name='knowledge_list'),
 
     url(r'^questions/(?P<question_id>\d+)/$',
-        'knowledge_thread', name='knowledge_thread_no_slug'),
+        knowledge_thread, name='knowledge_thread_no_slug'),
 
-    url(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', 'knowledge_list',
+    url(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', knowledge_list,
         name='knowledge_list_category'),
 
     url(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-_]+)/$',
-        'knowledge_thread', name='knowledge_thread'),
+        knowledge_thread, name='knowledge_thread'),
 
     url(r'^moderate/(?P<model>[a-z]+)/'
         r'(?P<lookup_id>\d+)/(?P<mod>[a-zA-Z0-9_]+)/$',
-        'knowledge_moderate', name='knowledge_moderate'),
+        knowledge_moderate, name='knowledge_moderate'),
 
-    url(r'^ask/$', 'knowledge_ask', name='knowledge_ask'),
+    url(r'^ask/$', knowledge_ask, name='knowledge_ask'),
 ]
